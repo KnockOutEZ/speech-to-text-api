dc4a3d42dd68566079941ae422f4cc41713b397d
KnockOutEZ <70368615+KnockOutEZ@users.noreply.github.com>
2022-09-26 22:43:35 +0600

    a bit of improvement done

--- demo.py
+++ demo.py
@@ -3,12 +3,44 @@ import webbrowser as wb
 r1 = sr.Recognizer()
 r2 = sr.Recognizer()
 r3 = sr.Recognizer()
+array = []
 
 with sr.Microphone() as source:
     print ('Speak Now')
-    audio = r3.listen(source)
+    audio = r2.listen(source)
+    result = r2.recognize_google(audio)
+    print (result)
 
-if 'Google' in r2.recognize_google(audio):
+
+if 'list' in r2.recognize_google(audio):
+    r2 = sr.Recognizer()
+    with sr.Microphone() as source:
+        print ('What do you wanna add?')
+        audio = r2.listen(source)
+        try:
+            output = r2.recognize_google(audio)
+            print (output)
+            array.append(output)
+            print(array)
+        except sr.UnknownValueError:
+            print ('error')
+        except sr.RequestError as e:
+            print ('failed' . format (e))
+
+
+if 'search' in r2.recognize_google(audio):
+    r2 = sr.Recognizer()
+    url = 'https://www.google.com/search?q='
+    try:
+        get = r2.recognize_google(audio).replace('search', '').replace('online', '')
+        print (get)
+        wb.get().open_new(url+get)
+    except sr.UnknownValueError:
+        print ('error')
+    except sr.RequestError as e:
+        print ('failed' . format (e))
+
+if 'search Google' in r2.recognize_google(audio):
     r2 = sr.Recognizer()
     url = 'https://www.google.com/search?q='
     with sr.Microphone() as source:
@@ -23,7 +55,7 @@ if 'Google' in r2.recognize_google(audio):
         except sr.RequestError as e:
             print ('failed' . format (e))
 
-if 'YouTube' in r1.recognize_google(audio):
+if 'search YouTube' in r1.recognize_google(audio):
     r1 = sr.Recognizer()
     url = 'https://www.youtube.com/results?search_query='
     with sr.Microphone() as source:
